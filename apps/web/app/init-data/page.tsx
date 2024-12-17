54d7a0e4739deb913b1768fa3e4dc693611dfaac
Nasim Akhtar <fnlog10@gmail.com>
2024-12-17 11:29:41 +0530

    feat: init data

---
+++ apps/web/app/init-data/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useSignal, initData } from "@telegram-apps/sdk-react";
+
+export default function InitDataPage() {
+  const initDataRaw = useSignal(initData.raw);
+  const initDataState = useSignal(initData.state);
+
+  return (
+    <div>
+      <h1>initData</h1>
+      <p>initData.raw: {JSON.stringify(initDataRaw)}</p>
+      <p>initData.state: {JSON.stringify(initDataState)}</p>
+    </div>
+  );
+}
---
+++ apps/web/utils/core.ts
@@ -0,0 +1,31 @@
+import {
+  backButton,
+  viewport,
+  themeParams,
+  miniApp,
+  initData,
+  $debug,
+  init as initSDK,
+} from "@telegram-apps/sdk-react";
+
+export function init(debug: boolean): void {
+  $debug.set(debug);
+
+  initSDK();
+
+  if (backButton.isSupported()) {
+    backButton.mount();
+  }
+
+  miniApp.mount();
+  themeParams.mount();
+  initData.restore();
+
+  void viewport.mount().catch((e) => {
+    console.error("Something went wrong mounting the viewport", e);
+  });
+
+  if (debug) {
+    import("eruda").then((lib) => lib.default.init()).catch(console.error);
+  }
+}
