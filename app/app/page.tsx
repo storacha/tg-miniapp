c70db118b784ce3809118cd6a1f06a41ec5b274a
Nasim Akhtar <fnlog10@gmail.com>
2024-12-22 16:33:31 +0530

    feat: cleanup

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,9 +1,3 @@
-import { Button } from "@repo/ui/components/ui/button";
-
 export default function Page() {
-  return (
-    <main className="h-screen flex justify-center items-center">
-      <Button>Click me</Button>
-    </main>
-  );
+	return <main className="h-screen flex justify-center items-center">{/* <Button>Click me</Button> */}</main>
 }
--- app/components/root.tsx
+++ app/components/root.tsx
@@ -1,43 +1,43 @@
-"use client";
+'use client'
 
-import type { PropsWithChildren } from "react";
-import { initData, useLaunchParams, useSignal } from "@telegram-apps/sdk-react";
-import { useDidMount } from "../hooks/useDidMount";
-import { ErrorBoundary } from "./error-boundary";
-import { ErrorPage } from "./error-page";
-import { useTelegramMock } from "../hooks/useTelegramMock";
-import { useClientOnce } from "../hooks/useClientOnce";
-import { init } from "../utils/core";
+import type { PropsWithChildren } from 'react'
+import { initData, useLaunchParams, useSignal } from '@telegram-apps/sdk-react'
+import { useDidMount } from '../hooks/useDidMount'
+import { ErrorBoundary } from './error-boundary'
+import { ErrorPage } from './error-page'
+import { useTelegramMock } from '../hooks/useTelegramMock'
+import { useClientOnce } from '../hooks/useClientOnce'
+import { init } from '../utils/core'
 
 function RootInner({ children }: PropsWithChildren) {
-  const isDev = process.env.NODE_ENV === "development";
+	const isDev = process.env.NODE_ENV === 'development'
 
-  if (isDev) {
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    useTelegramMock();
-  }
+	if (isDev) {
+		// eslint-disable-next-line react-hooks/rules-of-hooks
+		useTelegramMock()
+	}
 
-  const lp = useLaunchParams();
-  const debug = isDev || lp.startParam === "debug";
+	const lp = useLaunchParams()
+	const debug = isDev || lp.startParam === 'debug'
 
-  useClientOnce(() => {
-    init(debug);
-  });
+	useClientOnce(() => {
+		init(debug)
+	})
 
-  const initDataUser = useSignal(initData.user);
-  console.log("initDataUser", initDataUser);
-  //TODO: Add Global Context Provider
-  return <div>{children}</div>;
+	const initDataUser = useSignal(initData.user)
+	// console.log('initDataUser', initDataUser)
+	//TODO: Add Global Context Provider
+	return <div>{children}</div>
 }
 
 export function Root(props: PropsWithChildren) {
-  const didMount = useDidMount();
+	const didMount = useDidMount()
 
-  return didMount ? (
-    <ErrorBoundary fallback={ErrorPage}>
-      <RootInner {...props} />
-    </ErrorBoundary>
-  ) : (
-    <div className="root__loading">Loading</div>
-  );
+	return didMount ? (
+		<ErrorBoundary fallback={ErrorPage}>
+			<RootInner {...props} />
+		</ErrorBoundary>
+	) : (
+		<div className="root__loading">Loading</div>
+	)
 }
