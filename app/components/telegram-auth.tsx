c0026a405651edebcb6cfc926d9aba90de71509f
ash <alan138@gmail.com>
2025-04-10 18:24:18 +0100

    feat: client side login (#23)
    This simply logs in the client and saves the session in localstorage.
    
    We have to vendor `telegram` as there is a browser build step.
    
    Needs more work to expose the client globally.

--- app/components/telegram-auth.tsx
+++ app/components/telegram-auth.tsx
@@ -1,44 +1,19 @@
 'use client'
 
-import { useState, useEffect } from 'react'
-import { useLaunchParams } from '@telegram-apps/sdk-react'
+import { useState, useEffect, FormEventHandler } from 'react'
 import { Button } from '@/components/ui/button'
 import { InputOTP, InputOTPGroup, InputOTPSlot } from '@/components/ui/input-otp'
 import { useGlobal } from '@/zustand/global'
+import { Api } from '@/vendor/telegram'
+import { Input } from './ui/input'
+import { useTelegram } from '@/providers/telegram'
 
-// TODO: REMOVE THIS
-const TEST_PHONE_NUMBER = '+1234567890'
-
-async function requestOtp(){
-	console.log('Sending pin')
-	const {initDataRaw} = useLaunchParams()
-	
-	const response = await fetch('/api/telegram/session/request-otp', {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-			Authorization: `tma ${initDataRaw}`
-		},
-		body: JSON.stringify({
-			phoneNumber: TEST_PHONE_NUMBER, 
-		}),
-	});
-
-	if (!response.ok) {
-		throw new Error('Failed to send OTP');
-	}
-
-	// Handle the response (if needed)
-	const data = await response.json();
-	console.log('OTP sent successfully:', data);
-}
-
-function CountDown({ handleSendPin }: { handleSendPin: () => void }) {
+function CountDown({ onResend }: { onResend: () => unknown }) {
 	const [count, setCount] = useState(59)
 	const [resend, setResend] = useState(false)
 
 	function handleResend() {
-		handleSendPin()
+		onResend()
 		setCount(59)
 		setResend(false)
 	}
@@ -67,88 +42,137 @@ function CountDown({ handleSendPin }: { handleSendPin: () => void }) {
 	return <p className="text-xl font-semibold">00:{count}</p>
 }
 
-function SubmitOTP({ handleSendPin }: { handleSendPin: () => void }) {
-	const [OTP, setOTP] = useState<string | null>(null)
-	const [loading, setLoading] = useState(false)
-
-	const { setIsTgAuthorized } = useGlobal()
-
-	async function submitOTP() {
-		try {
-			setLoading(true)
-			setIsTgAuthorized(true)
-			setLoading(true)
-		} catch (error) {
-			setLoading(false)
-		}
+function OTPForm({ onSubmit, onCodeChange, onResend, code, loading, error }: { onSubmit: () => unknown, onCodeChange: (code: string) => unknown, onResend: () => unknown, code: string, loading: boolean, error?: Error }) {
+	const handleSubmit: FormEventHandler<HTMLFormElement> = e => {
+		e.preventDefault()
+		onSubmit()
 	}
 
-	const disabled = OTP === null || OTP.length < 4 || loading
+	const disabled = code === null || code.length < 5 || loading
 
 	return (
-		<div className="pt-20 pb-10 px-5 flex flex-col items-stretch justify-between h-screen bg-background">
+		<form onSubmit={handleSubmit} className="pt-20 pb-10 px-5 flex flex-col items-stretch justify-between h-screen bg-background">
 			<div className="flex flex-col items-center gap-2">
 				<h1 className="text-primary font-semibold text-2xl text-center">Storacha</h1>
 				<div className="py-10 flex flex-col items-center gap-5">
-					<InputOTP maxLength={4} onChange={(value) => setOTP(value)}>
+					<InputOTP maxLength={5} onChange={(value) => onCodeChange(value)}>
 						<InputOTPGroup>
 							<InputOTPSlot index={0} />
 							<InputOTPSlot index={1} />
 							<InputOTPSlot index={2} />
 							<InputOTPSlot index={3} />
+							<InputOTPSlot index={4} />
 						</InputOTPGroup>
 					</InputOTP>
+					{error && <p className="text-red-600 text-center text-xs my-2">{error.message}</p>}
 				</div>
 			</div>
 			<div className="flex flex-col items-center gap-5">
-				<CountDown handleSendPin={handleSendPin} />
+				<CountDown onResend={onResend} />
 				<p className="text-center text-blue-600/80">Hurry... enter the pin you received on your Telegram.</p>
 			</div>
 			<div className="flex justify-center items-center">
-				<Button className="w-full" onClick={submitOTP} disabled={disabled}>
+				<Button type="submit" className="w-full" disabled={disabled}>
 					{loading ? 'Loading...' : 'Submit OTP'}
 				</Button>
 			</div>
-		</div>
+		</form>
 	)
 }
 
 export default function TelegramAuth() {
-	const [isPinSended, setIsPinSended] = useState(false)
 	const [loading, setLoading] = useState(false)
+	const [error, setError] = useState<Error>()
+	const [codeHash, setCodeHash] = useState('')
+	const [code, setCode] = useState('')
+	const { setIsTgAuthorized, phoneNumber, setPhoneNumber } = useGlobal()
+	const [{ client }] = useTelegram()
 
-	async function handleSendPin() {
+	const handlePhoneSubmit: FormEventHandler<HTMLFormElement> = async e => {
+		e.preventDefault()
 		try {
 			setLoading(true)
-			await requestOtp()
-			setIsPinSended(true)
+			setError(undefined)
+
+			if (!client.connected) {
+				await client.connect()
+			}
+			const { phoneCodeHash } = await client.sendCode(client, phoneNumber)
+			setCodeHash(phoneCodeHash)
+		} catch (err: any) {
+			console.error('requesting OTP:', err)
+			setError(err)
+		} finally {
 			setLoading(false)
-		} catch (error) {
+		}
+	}
+	const handleCodeSubmit = async () => {
+		try {
+			setLoading(true)
+			setError(undefined)
+
+			if (!client.connected) {
+				await client.connect()
+			}
+			const result = await client.invoke(
+				new Api.auth.SignIn({
+					phoneNumber,
+					phoneCode: code,
+					phoneCodeHash: codeHash,
+				}),
+			)
+			if (result instanceof Api.auth.AuthorizationSignUpRequired) {
+				throw new Error('user needs to sign up')
+			}
+
+			setIsTgAuthorized(true)
+		} catch (err: any) {
+			console.error('signing in:', err)
+			setError(err)
+		} finally {
 			setLoading(false)
 		}
 	}
 
-	if (isPinSended) {
-		return <SubmitOTP handleSendPin={handleSendPin} />
+	const handleResend = async () => {
+		const { phoneCodeHash } = await client.sendCode(client, phoneNumber)
+		setCodeHash(phoneCodeHash)
+	}
+
+	if (codeHash) {
+		return <OTPForm code={code} onCodeChange={c => { setError(undefined); setCode(c) }} onSubmit={handleCodeSubmit} onResend={handleResend} loading={loading} error={error} />
 	}
 
 	return (
-		<div className="pt-20 pb-10 px-5 flex flex-col items-stretch justify-between h-screen bg-primary/10">
+		<form className="pt-20 pb-10 px-5 flex flex-col items-stretch justify-between h-screen bg-primary/10" onSubmit={handlePhoneSubmit}>
 			<div className="flex flex-col items-center gap-2">
 				<h1 className="text-primary font-semibold text-2xl text-center">Storacha</h1>
 				<div className="py-10 flex flex-col items-center gap-5">
 					<h1 className="text-xl font-semibold">Authorization</h1>
 					<p className="text-center text-blue-600/80">
-						Authorise access to your Telegram chats to securely proceed with your backups. You will receive a code in
-						Telegram. Please enter it in the next step to continue.
+						Authorise access to your Telegram chats to securely proceed with your backups.
 					</p>
+					<div className="w-full">
+						<p className="text-blue-600/80 text-center my-2">
+							Your phone number:
+						</p>
+						<Input className="bg-white" type="tel" placeholder="e.g. +12223334455" value={phoneNumber} onChange={e => { setError(undefined); setPhoneNumber(e.target.value) }} required />
+						{error ? (
+							<p className="text-red-600 text-center text-xs my-2">{error.message}</p> 
+						) : (
+							<p className="text-blue-600/80 text-center text-xs my-2">Please enter your number in <a href="https://telegram.org/faq#login-and-sms" target="_blank" className='underline'>international format</a>.</p>
+						)}
+					</div>
 				</div>
 			</div>
-			<div className="flex justify-center items-center">
-				<Button className="w-full" onClick={handleSendPin} disabled={loading}>
+			<div className="flex flex-col justify-center items-center">
+				<p className="text-center text-blue-600/80 mb-3">
+					You will receive a code in Telegram. Please enter it in the next step to continue.
+				</p>
+				<Button type="submit" className="w-full" disabled={loading || !phoneNumber}>
 					{loading ? 'Sending...' : 'Send Pin'}
 				</Button>
 			</div>
-		</div>
+		</form>
 	)
 }
