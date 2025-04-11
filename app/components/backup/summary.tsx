c8cad6790e0c725100d8b35662000ed3c2662026
Alan Shaw <alan@storacha.network>
2025-04-11 17:30:23 +0100

    fix: style fixes

--- app/components/backup/summary.tsx
+++ app/components/backup/summary.tsx
@@ -18,19 +18,23 @@ export const Summary = ({ chats, space, period, onSubmit }: SummaryProps) => {
   return (
     <form onSubmit={handleSubmit}>
       <div className="w-full pt-0 px-5 flex flex-col text-center justify-center gap-2 pb-5">
-				<h1 className="text-lg font-semibold text-foreground text-center">Summary</h1>
+				<h1 className="text-lg font-semibold text-foreground text-center">Ready?</h1>
+        <p className="text-sm">Check the details before we start.</p>
 			</div>
 			<div className="flex flex-col gap-5 rounded-t-xl bg-background w-full flex-grow py-2">
-        <p>{chats.size} Chats</p>
-        <p>Destination: {space}</p>
-        {period[0] === 0 && period[1] === Infinity ? (
-          <p>Period: All time</p>
-        ) : (
-          <>
-            <p>From: {new Date(period[0]).toLocaleString()}</p>
-            <p>To: {new Date(period[1]).toLocaleString()}</p>
-          </>
-        )}
+        <div className="flex space-x-2 items-center gap-2 border-b border-primary/10 p-5">
+          <p>{chats.size.toLocaleString()} Chat{chats.size === 1 ? '' : 's'}</p>
+        </div>
+        <div className="flex space-x-2 items-center gap-2 border-b border-primary/10 p-5">
+          {period[0] === 0 && period[1] === Infinity ? (
+            <p>Period: All time</p>
+          ) : (
+            <>
+              <p>From: {new Date(period[0]).toLocaleDateString()}</p>
+              <p>To: {new Date(period[1]).toLocaleDateString()}</p>
+            </>
+          )}
+        </div>
         <div className="sticky bottom-0 w-full p-5">
           <Button type="submit" className="w-full">Start Backup</Button>
         </div>
