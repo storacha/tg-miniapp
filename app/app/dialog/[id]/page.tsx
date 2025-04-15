c2475b13a25ee510e3e803beffa4ed430c93c89a
Alan Shaw <alan@storacha.network>
2025-04-15 23:52:09 +0100

    chore: appease linter

--- app/app/dialog/[id]/page.tsx
+++ app/app/dialog/[id]/page.tsx
@@ -1,9 +1,6 @@
 'use client'
 
 import { Layouts } from '@/components/layouts'
-import { Banner } from '@/components/leaderboard/banner'
-import { Podium } from '@/components/leaderboard/podium'
-import Users from '@/components/leaderboard/users'
 import { useParams } from 'next/navigation'
 
 export default function Page () {
