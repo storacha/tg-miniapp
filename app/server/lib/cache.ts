239c2e400ed2eed986eddbab50aa03ab44b76081
Alan Shaw <alan@storacha.network>
2025-04-15 11:23:39 +0100

    chore: purge unused server code from app directory

--- app/server/lib/cache.ts
+++
@@ -1,7 +0,0 @@
-export async function getSession(userId: string): Promise<string | undefined> {
-    return global.sessionCache.get(userId)
-}
-
-export async function setSession(userId: string, session: any){
-    global.sessionCache.set(userId, session)
-}
