e0db54dc413ee1fe3f8ce63ef2d46ca9ff83c7e2
bravonatalie <natalie.bravo@outlook.com>
2025-04-18 09:49:34 +0200

    fix: adjust message dialogs and thumbnail

--- app/app/dialog/[id]/page.tsx
+++ app/app/dialog/[id]/page.tsx
@@ -1,27 +1,31 @@
 'use client'
 
 import { CAR } from '@ucanto/core'
-import { CID } from 'multiformats'
-import { CarIndexer } from '@ipld/car'
 import * as dagCBOR from '@ipld/dag-cbor'
 import { useEffect, useState } from 'react'
-import { exporter } from 'ipfs-unixfs-exporter'
-import { MemoryBlockstore } from 'blockstore-core'
 import { useTelegram } from '@/providers/telegram'
 import { cloudStorage} from '@telegram-apps/sdk-react'
 import { useParams, useSearchParams } from 'next/navigation'
 
 import * as Crypto from '@/lib/crypto'
-import { Layouts } from '@/components/layouts'
 import { Media } from '@/components/ui/media'
+import { Layouts } from '@/components/layouts'
 import { useBackups } from '@/providers/backup'
+import { decodeStrippedThumb, toJPGDataURL } from '@/lib/utils'
 import { BackupModel, DialogData, EntityData, MessageData } from '@/api'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
-import { toJPGDataURL } from '@/lib/utils'
+
+/**
+ * TODO:
+ * [] something is wrong with the message.from id, it's not the same as the user.id for the same user
+ * [x] thumbnail
+ * [x] text out of the box
+ */
 
 export const runtime = 'edge'
 
 type BackupDialogProps = {
+  userId: number
   dialog: DialogData
   messages: MessageData[]
   participants: Record<string, EntityData>
@@ -29,22 +33,35 @@ type BackupDialogProps = {
 
 const formatDate = (timestamp: number) => (new Date(timestamp * 1000)).toLocaleString()
 
+const getInitials = (name: string) => {
+  const words = name.trim().split(' ')
+  if (words.length === 1) {
+    return words[0][0].toUpperCase() 
+  }
+  return (words[0][0] + words[1][0]).toUpperCase()
+}
+
 export function BackupDialog({
+  userId,
   dialog,
   messages,
   participants,
 }: BackupDialogProps) {
   const sortedMessages = [...messages].sort((a, b) => a.date - b.date)
-  const selfId = dialog.id
   
-
+  let dialogThumbSrc = ''
+  if(dialog.photo?.strippedThumb){
+    dialogThumbSrc = toJPGDataURL(decodeStrippedThumb(dialog.photo?.strippedThumb))
+  }
+  
   return (
     <div className="flex flex-col h-screen bg-background">
       {/* Header */}
       <div className="flex items-center gap-4 px-5 py-4 border-b border-border bg-muted">
-        <div className="h-10 w-10 rounded-full bg-gray-300 flex items-center justify-center text-sm font-semibold text-white">
-          {dialog.name.slice(0, 2).toUpperCase()}
-        </div>
+          <Avatar>
+            <AvatarImage src={dialogThumbSrc} />
+            <AvatarFallback>{getInitials(dialog.name)}</AvatarFallback>
+          </Avatar>
         <div>
           <h1 className="text-lg font-semibold text-foreground">{dialog.name}</h1>
           <p className="text-sm text-muted-foreground capitalize">{dialog.type}</p>
@@ -53,24 +70,32 @@ export function BackupDialog({
 
       {/* Messages */}
       <div className="flex-1 overflow-y-auto px-4 py-6 space-y-4">
-        {sortedMessages.map((msg) => {
-          const isOutgoing = msg.from === selfId
+        {sortedMessages.map((msg, index) => {
+          const isOutgoing = msg.from === '883154390'
+          // console.log('msg.from: ', msg.from)
+          // console.log('userId: ', String(userId))
+          // console.log('isOutgoing: ', isOutgoing)
           const sender = participants[msg.from]?.name ?? 'Unknown'
+
+          const showSenderHeader =
+            !isOutgoing &&
+            (index === 0 || sortedMessages[index - 1].from !== msg.from)
+
           let thumbSrc = ''
+          // console.log('strippedThumb:', participants[msg.from]?.photo?.strippedThumb)
           if(participants[msg.from].photo?.strippedThumb){
-           thumbSrc = toJPGDataURL(participants[msg.from].photo?.strippedThumb!)
+           thumbSrc = toJPGDataURL(decodeStrippedThumb(participants[msg.from].photo?.strippedThumb!))
           }
-          console.log(JSON.stringify(msg))
-          console.log(JSON.stringify(participants[msg.from].photo))
+          console.log('thumbSrc: ', thumbSrc)
 
           return (
             <div key={msg.id} className={`flex ${isOutgoing ? 'justify-end' : 'justify-start'}`}>
               <div className="flex flex-col max-w-[75%]">
-                {!isOutgoing && (
+                {showSenderHeader && (
                   <div className="flex items-center gap-2 mb-2">
                     <Avatar>
                       <AvatarImage src={thumbSrc} />
-                      <AvatarFallback>{sender.substring(0,1)}</AvatarFallback>
+                      <AvatarFallback>{getInitials(sender)}</AvatarFallback>
                     </Avatar>
                     <p className="text-xs text-muted-foreground font-medium">
                       {sender}
@@ -78,7 +103,7 @@ export function BackupDialog({
                   </div>
                 )}
                 <div
-                  className={`px-4 py-2 text-sm rounded-xl whitespace-pre-line shadow-sm ${
+                  className={`px-4 py-2 text-sm rounded-xl whitespace-pre-line shadow-sm break-words ${
                     isOutgoing
                       ? 'bg-blue-500 text-white rounded-br-none'
                       : 'bg-gray-100 text-foreground rounded-bl-none'
@@ -88,7 +113,11 @@ export function BackupDialog({
                     (<p>{msg.message}</p>) :
                     (<Media  />)
                   }
-                  <div className="text-xs text-muted-foreground text-right mt-1">
+                  <div className={`text-xs text-right mt-1 ${
+                    isOutgoing
+                      ? 'text-gray-300'
+                      : 'text-muted-foreground'
+                    }`}>
                     {formatDate(msg.date)}
                   </div>
                 </div>
@@ -111,7 +140,7 @@ const getFromStoracha = async (cid: string) => {
 }
 
 export default function Page () {
-  const [{ client }] = useTelegram()
+  const [{ client, user }] = useTelegram()
   const [{ backups, dialog }] = useBackups()
   const [loading, setLoading] = useState(true)
   const [dialogData, setDialogData] = useState<DialogData | null>(null)
@@ -198,26 +227,22 @@ export default function Page () {
       } catch (error) {
         console.error('Error in useEffect:', error)
       }
-    }
-    restoreBackup()
+      }
+      restoreBackup()
       return () => { cancel = true }
     }, [client, backupCid, params.id])
 
-  if ( !dialogData) { 
-    return (
-      <Layouts isSinglePage isBackgroundBlue>
+  return (
+    <Layouts isSinglePage isBackgroundBlue>
+      {loading && <p className='text-center'>Loading...</p> }
+      {!loading && !dialogData && 
         <div className="flex flex-col items-center justify-center h-screen">
           <p className="text-lg font-semibold text-red-500">Dialog not found</p>
         </div>
-      </Layouts>
-    );
-  }
-
-  return (
-    <Layouts isSinglePage isBackgroundBlue>
-      { dialogData && 
-        <BackupDialog dialog={dialogData} messages={messages} participants={participants} />
       }
-      </Layouts>
+      {dialogData && 
+        <BackupDialog userId={user?.id!} dialog={dialogData} messages={messages} participants={participants} />
+      }
+    </Layouts>
   )
 }
