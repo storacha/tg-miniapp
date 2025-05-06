0edf78b0e8996989fcfb914937e1d4a280e025ec
bravonatalie <natalie.bravo@outlook.com>
2025-05-06 12:50:52 -0300

    chore: improve select backup screen

--- app/app/dialog/[id]/page.tsx
+++ app/app/dialog/[id]/page.tsx
@@ -23,26 +23,33 @@ export default function BackupSelectionPage() {
 
   return (
     <Layouts isSinglePage>
-        <div className="flex flex-col gap-5 min-h-screen">
-        <h1 className="px-5">Select a Backup</h1>
-        <div className="flex flex-col">
-            {dialogBackups.map((backup) => (
-            <div
-                key={backup.data.toString()}
-                className="flex justify-between items-center px-5 py-3 border-b border-border cursor-pointer hover:bg-muted"
-                onClick={(e) => handleBackupClick(e, backup.data.toString())}
-            >
-                <div>
-                <p className="text-sm text-foreground/60">Backup Date:</p>
-                <p className="text-lg font-semibold text-foreground">
-                    {new Date(backup.params.period[1] * 1000).toLocaleString()}
-                </p>
-                </div>
-                <ChevronRight />
+  <div className="flex flex-col gap-5 min-h-screen">
+    <h1 className="px-5 text-xl font-bold">Select a Backup</h1>
+    <div className="flex flex-col">
+      {dialogBackups.map((backup, index) => {
+        const fromDate = new Date(backup.params.period[0] * 1000).toLocaleDateString()
+        const toDate = new Date(backup.params.period[1] * 1000).toLocaleDateString()
+        const sharedTime = new Date(backup.params.period[0] * 1000).toLocaleTimeString([])
+
+        return (
+          <div
+            key={backup.data.toString()}
+            className={`flex justify-between items-center px-5 py-3 border-b border-border cursor-pointer hover:bg-muted ${
+              index === 0 ? 'rounded-t-lg' : ''
+            } ${index === dialogBackups.length - 1 ? 'rounded-b-lg' : ''}`}
+            onClick={(e) => handleBackupClick(e, backup.data.toString())}
+          >
+            <div className="flex flex-col">
+              <p className="text-sm text-foreground/60">{`from: ${fromDate}`}</p>
+              <p className="text-base font-medium text-foreground">{`to: ${toDate}`}</p>
+              <p className="text-sm text-foreground/60">{`at: ${sharedTime}`}</p>
             </div>
-            ))}
-        </div>
-        </div>
-    </Layouts>
+            <ChevronRight className="text-muted-foreground" />
+          </div>
+        );
+      })}
+    </div>
+  </div>
+</Layouts>
   )
 }
