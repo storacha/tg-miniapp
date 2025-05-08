3bc1b1eb7a6f75a4b4bf9227c3996d314a224328
hannahhoward <hannah@hannahhoward.net>
2025-05-07 20:00:27 -0700

    feat(server): add new server architecture, with no actual api

--- server/src/config/configuration.ts
+++
@@ -1,9 +0,0 @@
-export default () => ({
-  port: process.env.PORT || 3000,
-  mongodb: {
-    uri: process.env.MONGODB_URI || 'mongodb://localhost/my-enterprise-db',
-  },
-  swagger: {
-    path: process.env.SWAGGER_PATH || 'api',
-  },
-});
