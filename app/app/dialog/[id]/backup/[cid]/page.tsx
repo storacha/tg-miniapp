820a06f585e66eb85a26a094904ba71f0eaee27e
kaf-lamed-beyt <belac335@gmail.com>
2025-06-24 16:22:56 +0100

    chore: show header component when a backed up chat is loading messages
    this is so people can navigate back to the previous page if the loading is endless

--- app/app/dialog/[id]/backup/[cid]/page.tsx
+++ app/app/dialog/[id]/backup/[cid]/page.tsx
@@ -319,7 +319,7 @@ export default function Page() {
   }
 
   return (
-    <Layouts isSinglePage withHeader={false}>
+    <Layouts isSinglePage withHeader={restoredBackup.loading}>
       {restoredBackup.error && (
         <ConnectError
           open={!!restoredBackup.error}
