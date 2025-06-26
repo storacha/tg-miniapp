b3fda5986a4b760d7f69d2149c3137e31a31706f
Travis Vachon <travis.vachon@gmail.com>
2025-06-25 17:57:36 -0700

    feat: configure sentry

---
+++ app/lib/sentry.ts
@@ -0,0 +1,11 @@
+import { captureException } from '@sentry/nextjs'
+
+/**
+ * Log to the error console and capture the error in Sentry.
+ *
+ * @param err the error - typed as unknown to match catch(err)
+ */
+export function logAndCaptureError(err: unknown) {
+  console.error(err)
+  captureException(err)
+}
