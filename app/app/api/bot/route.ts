350a1a35920bf418d2e24a1b393701fd576fb3cb
bravonatalie <natalie.bravo@outlook.com>
2025-06-27 11:03:04 -0300

    chore: add app url to api/bot route

--- app/app/api/bot/route.ts
+++ app/app/api/bot/route.ts
@@ -1,5 +1,10 @@
 import { NextRequest, NextResponse } from 'next/server'
 
+const appUrl =
+  process.env.NODE_ENV === 'production'
+    ? 'https://telegram.storacha.network'
+    : process.env.LOCAL_URL
+
 export async function POST(req: NextRequest) {
   const body = await req.json()
 
@@ -10,19 +15,19 @@ export async function POST(req: NextRequest) {
   if (text === '/start' && chatId) {
     const telegramApiUrl = `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendMessage`
 
-    await fetch(telegramApiUrl, {
+    const response = await fetch(telegramApiUrl, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
         chat_id: chatId,
-        text: 'Welcome! Tap the button below to open the app.',
+        text: 'Welcome to Secure Chats Backup!\nClick the button below to start backing up your chats.',
         reply_markup: {
           inline_keyboard: [
             [
               {
-                text: 'Launch',
+                text: 'Open',
                 web_app: {
-                  url: process.env.LOCAL_URL,
+                  url: appUrl,
                 },
               },
             ],
@@ -32,6 +37,11 @@ export async function POST(req: NextRequest) {
         },
       }),
     })
+
+    if (!response.ok) {
+      console.error('Failed to send message:', response.statusText)
+      return NextResponse.json({ ok: false, error: 'Failed to send message' })
+    }
   }
 
   return NextResponse.json({ ok: true })
