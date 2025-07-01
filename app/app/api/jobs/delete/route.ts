9acce748ef907a83059380730abfb8a0834379a2
kaf-lamed-beyt <belac335@gmail.com>
2025-07-01 14:29:10 +0100

    remove unnecessary API route since i figured out the server/client mismatch issue

--- app/app/api/jobs/delete/route.ts
+++
@@ -1,8 +0,0 @@
-import { deleteJob } from '@/lib/server/jobs'
-import { NextResponse } from 'next/server'
-
-export async function POST(req: Request) {
-  const { jobID } = await req.json()
-  await deleteJob({ jobID })
-  return NextResponse.json({ success: true })
-}
