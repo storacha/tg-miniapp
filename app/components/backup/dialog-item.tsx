2d67eb28f144502c6acf08b7665a8c55bafefc38
Travis Vachon <travis.vachon@gmail.com>
2025-07-10 20:37:55 -0700

    Revert "Merge pull request #224 from storacha/feat/hq-images"
    This reverts commit 7e2ef50febf177ad683474c5adf368795c1979bf, reversing
    changes made to ee9e0d42d54274a0e1b74deec2615dcbc88cf0bd.

--- app/components/backup/dialog-item.tsx
+++ app/components/backup/dialog-item.tsx
@@ -2,54 +2,15 @@ import { AbsolutePeriod, DialogInfo } from '@/api'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { useUserLocale } from '@/hooks/useUserLocale'
 import { getThumbSrc } from '@/lib/backup/utils'
-import { useGlobal } from '@/zustand/global'
-import { useEffect, useState } from 'react'
 
 interface DialogItemProps {
   dialog: DialogInfo
   latestBackup?: AbsolutePeriod
 }
 
-// Try to fetch the high quality photo for the given entity.
-// If it works, the image will be cached and can be passed to an image src url.
-function useHighQualityProfilePhoto(
-  entityId: string,
-  accessHash?: string
-): string | undefined {
-  const [hqUrl, setHqUrl] = useState<string>()
-  const { tgSessionString } = useGlobal()
-  useEffect(
-    function () {
-      console.log('loading HQ for id ', entityId)
-      if (entityId) {
-        ;(async function () {
-          // session is required for this to work unless it's been cached
-          // but we keep it in a query param so that browsers with support for
-          // No-Vary-Search ignore it when considering whether to cache the image.
-          const query: { access?: string; session: string } = {
-            session: tgSessionString,
-          }
-          if (accessHash) query.access = accessHash
-          const queryString = new URLSearchParams(query).toString()
-          const url = `/api/entity/${encodeURIComponent(entityId)}/image${queryString ? `?${queryString}` : ''}`
-          const result = await fetch(url)
-          if (result.status === 200) {
-            setHqUrl(url)
-          }
-        })()
-      }
-    },
-    [entityId]
-  )
-  return hqUrl
-}
-
 export const DialogItem = ({ dialog, latestBackup }: DialogItemProps) => {
-  const { id, name, initials, photo, dialogId, accessHash } = dialog
-  const lqThumbSrc = getThumbSrc(photo?.strippedThumb)
-  const hqThumbSrc =
-    dialogId && useHighQualityProfilePhoto(dialogId, accessHash)
-
+  const { name, initials, photo } = dialog
+  const thumbSrc = getThumbSrc(photo?.strippedThumb)
   const { formatDateTime } = useUserLocale()
   const latestBackupDate = latestBackup
     ? formatDateTime(Number(latestBackup[1]))
@@ -58,7 +19,7 @@ export const DialogItem = ({ dialog, latestBackup }: DialogItemProps) => {
   return (
     <div className="flex gap-4 items-center w-full">
       <Avatar className="flex-none">
-        <AvatarImage src={hqThumbSrc || lqThumbSrc} />
+        <AvatarImage src={thumbSrc} />
         <AvatarFallback>{initials}</AvatarFallback>
       </Avatar>
       <div className="flex-auto">
